fe4ae04433013b49d949d122fffc29fbb79c74af
Paolo <paolo.alhambra@yahoo.com>
2023-05-01 11:10:41 -0700

    New blog post working properly

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -8,10 +8,11 @@ router.post('/', withAuth, async (req, res) => {
   const newBlog = await Blog.create({
     title: req.body.title,
     content: req.body.content,
-    username: req.session.user.name,
+    username: req.session.username,
     date_created: new Date()
   });
-  res.status(200).json(newBlog);
+  res.redirect('/dashboard');
+  // res.status(200).json(newBlog);
 } catch (err) {
   res.status(500).json(err)
 }
